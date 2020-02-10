6caa13f8c21612df6cc7e359d3f47467793a18e4
Silje <silje.susort@gmail.com>
2020-02-10 15:53:39 +0100

    Made order.c, deleted door module

--- source/order.h
+++ source/order.h
@@ -4,25 +4,39 @@
  *
  */
 
+#include "hardware.h"
 
 struct Order {
 
     int floor;
-    int floor_light_indicator_init;
-    int valid;
-
-    /**
-     * @brief Initializes valid order.
-     *
-     * @return 0 on success. Non-zero for failure.
-     */
-    int order_init();
-
-    /**
-     * @brief Deletes order.
-     *
-     * @return 0 on success. Non-zero for failure.
-     */
-    int order_delete();
+    HardwareOrder order_type;
+    bool emptyOrder;
 
 };
+
+/**
+ * @brief Initializes valid order.
+ *
+ * @return 0 on success. Non-zero for failure.
+ */
+struct Order order_init(int floor, HardwareOrder order_type);
+
+/**
+ * @brief Turns off corresponing light.
+ *
+ */
+void order_turn_off_light(struct Order order);
+
+/**
+ * @brief Deactivates order by setting emptyOrder to true.
+ *
+ * @return returns deactivated order.
+ */
+struct Order order_make_empty(struct Order order);
+
+/**
+ * @brief Deletes order by deactivating and turning off order light.
+ *
+ * @return returns deleted order.
+ */
+struct Order order_delete(struct Order order);
